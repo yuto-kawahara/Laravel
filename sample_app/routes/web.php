16caf96df925a1e09f6c9f5e541dd4e29da8b315
yuto-kawahara <mashroom1384@gmail.com>
2021-12-18 12:41:12 +0900

    [Add]Hello World

--- sample_app/routes/web.php
+++ sample_app/routes/web.php
@@ -14,3 +14,68 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+
+// $html = <<<EOF
+// <html>
+//     <head>
+//         <title>Hello</title>
+//         <style>
+//             body {
+//                 font-size: 16pt;
+//                 color: #999;
+//             }
+//             h1 {
+//                 font-size: 100pt;
+//                 text-align:right;
+//                 color: #eee;
+//                 margin: -40px 0px -50px 0px;
+//             }
+//         </style>
+//     </head>
+//     <body>
+//         <h1>Hello</h1>
+//         <p>This is sample page.</p>
+//         <p>これはサンプルで作ったページです。</p>
+//     </body>
+// </html>
+// EOF;
+
+// Route::get('hello', function () {
+//     return '<html><body><h1>Hello</h1><p>This is sample page.</p></body></html>';
+// });
+// Route::get('hello', function () use ($html) {
+//     return $html;
+// });
+// Route::get('hello/{msg}', function ($msg) {
+//     $html = <<<EOF
+//     <html>
+//         <head>
+//             <title>Hello</title>
+//             <style>
+//                 body {
+//                     font-size: 16pt;
+//                     color: #999;
+//                 }
+//                 h1 {
+//                     font-size: 100pt;
+//                     text-align:right;
+//                     color: #eee;
+//                     margin: -40px 0px -50px 0px;
+//                 }
+//             </style>
+//         </head>
+//         <body>
+//             <h1>Hello</h1>
+//             <p>{$msg}</p>
+//             <p>これはサンプルで作ったページです。</p>
+//         </body>
+//     </html>
+//     EOF;
+    
+//     return $html;
+// });
+
+Route::get('hello', 'HelloController@index');
+Route::get('hello/other', 'HelloController@other');
