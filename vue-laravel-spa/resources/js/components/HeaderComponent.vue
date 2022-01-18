0c407716a07c951920e017dbe922e947af72c154
yuto-kawahara <mashroom1384@gmail.com>
2022-01-18 22:05:02 +0900

    [Add]tailwindcssの設定確認

--- vue-laravel-spa/resources/js/components/HeaderComponent.vue
+++ vue-laravel-spa/resources/js/components/HeaderComponent.vue
@@ -1,15 +1,15 @@
 <template>
     <div class="container-fluid bg-dark mb-3">
-        <div class="container">
-            <nav class="navbar navbar-dark">
-                <span class="navbar-brand mb-0 h1">Vue Laravel SPA</span> +
+        <div class="">
+            <nav
+                class="flex text-white bg-black py-2 px-4 justify-between items-center w-full"
+            >
+                <span class="navbar-brand mb-0 h1">Vue Laravel SPA</span>
                 <div>
-                    <button class="btn">List</button>
-                    <button
-                        class="py-2 px-4 text-blue-500 border border-blue-500 mr-5 hover:bg-gray-100 hover:border-blue-300 rounded"
-                    >
-                        ADD
-                    </button>
+                    <router-link v-bind:to="{ name: 'task.list' }">
+                        <button class="btn">List</button>
+                    </router-link>
+                    <button class="btn">ADD</button>
                 </div>
             </nav>
         </div>
